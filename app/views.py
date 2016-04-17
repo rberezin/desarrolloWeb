4da0bead140948aa206d429a4bcb02dc7c2e1b87
Alejandro Vega <ravp_92@hotmail.com>
2016-04-16 21:15:14 -0300

    Estructura de Archivos
    app\
    static\
    templates\
    __init__.py
    views.py
    tmp\
    run.py

---
+++ app/views.py
@@ -0,0 +1,22 @@
+import flask
+from app import app
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Miguel'}
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return flask.render_template("index.html",
+                                 title='Home',
+                                 user=user,
+                                 posts=posts)
